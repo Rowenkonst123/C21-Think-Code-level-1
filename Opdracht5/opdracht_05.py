998c5a9007b3b83da1fedcdad8d4f6af79523dd1
Rowen Konst <rowen.konst@student.rocva.nl>
2022-02-18 10:44:34 +0100

    opdrac ht 5 complete

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,10 @@
 #maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+
+def favoriete_games(Name):
+    print("favoriete games", Name)
+
+favoriete_games("Lost Ark")
+favoriete_games("Call of Duty")
+favoriete_games("Rocket League")
